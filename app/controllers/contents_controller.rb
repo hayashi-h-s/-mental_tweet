b03d6d23a6b3b3ec3f74d2f4967745c7681cf8fa
hayamizhin <max9173627>
2019-10-03 10:44:31 +0900

    アプリ新規作成

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,27 @@
+class ContentsController < ApplicationController
+  def index
+    @contents = Content.all
+  end
+
+  def new
+    @content = Content.new
+  end
+
+  def create
+    @content =Content.new(content_params)
+    @content.save!
+    redirect_to contents_path,notice: "コンテンツ『#{content.title}』を投稿しました。"
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:title)
+  end
+end
