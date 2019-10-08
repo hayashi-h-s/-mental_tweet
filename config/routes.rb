efefde537f7d664c0656476905737b078274a796
hayamizhin <max9173627>
2019-10-08 14:56:42 +0900

    contents_id追加のマイグレーション前

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  root to: 'home#top'
+
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+
   namespace :admin do
     resources :users
   end
-  root to: 'home#top'
+
   resources :contents
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
